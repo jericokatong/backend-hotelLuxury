e40ed658bd1bc794c160c8fa0692b99c020a4248
jericokatong <jericokatong@gmail.com>
2023-06-12 16:21:41 +0800

    mencoba ubah public lagi

--- app/Admin/router.js
+++ app/Admin/router.js
@@ -2,7 +2,7 @@ const { getAdmin, registerAdmin, LoginAdmin, LogoutAdmin } = require("./controll
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "/tmp" });
+const upload = multer({ dest: "/public/images" });
 
 router.get("/admin", getAdmin);
 router.post("/admin/register", upload.single("file"), registerAdmin);
